cf8c67a08c81a2f8dc11cb585df010bbf4ad16a1
tatyanakuchuk <tatyana8787@gmail.com>
2024-02-13 10:57:28 +0100

    Add comments to the functions in main.js

---
+++ Readme.md
@@ -0,0 +1,8 @@
+## Step 1
+- create a repository: run `git init` in this directory. Git creates the.git directory, which is where almost everything that Git stores and manipulates is located.
+
+- create a branch and switch to it.
+
+-in the main.js file, write a utility function that will return a random integer from the given range, inclusive. The range should be positive, including zero. Also, consider how the function should behave if the 'from' value is less than the 'to' value or equal to it.
+
+-write a function to check the maximum length of a string. It will be used to validate the length of entered comments but should be universal. The result should be either true or false.
--- js/main.js
+++ js/main.js
@@ -1,15 +1,27 @@
+/**
+ * Return random integer numberin range from min to max.
+ *
+ * @param {number} min integer positive number.
+ * @param {number} max integer positive number.
+ * @return {number} random integer number in range from min to max.
+ */
+
 const getRandomInt = (min, max) => {
   const minCeiled = Math.ceil(min);
   const maxFloored = Math.floor(max);
   return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
 }
 
-console.log(getRandomInt(12, 9));
+/**
+ * Validate the length of string.
+ *
+ * @param {string} string, some text.
+ * @param {number} maxLength - integer positive number.
+ * @return {boolean} true if string less or equal maxLength or false if more than maxLength.
+ */ 
 
 const checkStringLength = (string, maxLength) => {
   return string.trim().length <= maxLength;
 }
-
-console.log(checkStringLength('написать функцию для проверки ', 5))
 
 
